cb9edc9e738425647de6358d9da9799a074ee990
asrata <nju.angli@gmail.com>
2015-03-12 14:49:18 -0400

    added scripts for installing facebook torch tools

---
+++ install_fbtools.sh
@@ -0,0 +1,74 @@
+#!/bin/bash -e
+#
+#  Copyright (c) 2014, Facebook, Inc.
+#  All rights reserved.
+#
+#  This source code is licensed under the BSD-style license found in the
+#  LICENSE file in the root directory of this source tree. An additional grant
+#  of patent rights can be found in the PATENTS file in the same directory.
+#
+
+echo
+echo This script will install fblualib and all its dependencies.
+echo It has been tested on Ubuntu 13.10 and Ubuntu 14.04, Linux x86_64.
+echo
+
+if [[ $(arch) != 'x86_64' ]]; then
+    echo "x86_64 required" >&2
+    exit 1
+fi
+
+issue=$(cat /etc/issue)
+extra_packages=
+
+dir=$(mktemp -d fblualib-build.XXXXXX)
+
+echo Working in $dir
+echo
+cd $dir
+
+echo
+echo Cloning repositories
+echo
+git clone https://github.com/facebook/folly
+git clone https://github.com/facebook/fbthrift
+git clone https://github.com/facebook/thpp
+git clone https://github.com/soumith/fblualib
+
+echo
+echo Building folly
+echo
+
+cd $dir/folly/folly
+autoreconf -ivf
+./configure --prefix=$HOME/local
+make
+make install
+
+echo
+echo Building fbthrift
+echo
+
+cd $dir/fbthrift/thrift
+autoreconf -ivf
+./configure --prefix=$HOME/local
+make
+make install
+
+echo
+echo 'Installing TH++'
+echo
+
+cd $dir/thpp/thpp
+./build.sh
+
+echo
+echo 'Installing FBLuaLib'
+echo
+
+cd $dir/fblualib/fblualib
+./build.sh
+
+echo
+echo 'All done!'
+echo
